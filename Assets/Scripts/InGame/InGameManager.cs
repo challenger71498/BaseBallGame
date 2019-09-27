741772a7aa2286c08110752c60bcaa3139f4b5ca
challenger71498 <challenger71498@gmail.com>
2019-09-27 20:52:59 +0900

    Changed variable name savedPitcher to otherPitcher
    The variable named as savedPitcher is used for save attacker's pitcher while not pitching. But naming is kinda makes a confusion between pitcher who saves. So Changed name to otherPitcher.

--- Assets/Scripts/InGame/InGameManager.cs
+++ Assets/Scripts/InGame/InGameManager.cs
@@ -14,7 +14,7 @@ public class InGameManager : MonoBehaviour
     public static Team currentAttack;
     public static Team currentDefend;
     public static Pitcher currentPitcher;
-    public static Pitcher savedPitcher;
+    public static Pitcher otherPitcher;
     public static Batter currentBatter;
     public static List<KeyValuePair<int, Batter>> homeBattingOrder;
     public static List<KeyValuePair<int, Batter>> awayBattingOrder;
@@ -452,7 +452,7 @@ public class InGameManager : MonoBehaviour
         }
 
         //Switch side.
-        currentPitcher = savedPitcher;
+        currentPitcher = otherPitcher;
 
         if (currentAttack == game.home)
         {
