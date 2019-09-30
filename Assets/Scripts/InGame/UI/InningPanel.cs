8ca372b77149cdfc79daed6a6cb75a7280ee1f37
challenger71498 <challenger71498@gmail.com>
2019-10-01 04:29:28 +0900

    InGame InningPanel Management
    This is for inningPanel gameobject.

---
+++ Assets/Scripts/InGame/UI/InningPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InningPanel : MonoBehaviour
+{
+    public InGameObjects inGameObjects;
+
+    public void UpdateLayout()
+    {
+        inGameObjects.inningText.text = InGameManager.currentInning.ToString();
+        if (!InGameManager.isBottom) //Top
+        {
+            inGameObjects.inningImage.rectTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            inGameObjects.inningImage.rectTransform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        
+    }
+}
