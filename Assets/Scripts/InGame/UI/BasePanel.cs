5925f20f7cbfae487f513c1bd94071c52b03c671
challenger71498 <challenger71498@gmail.com>
2019-10-01 04:28:59 +0900

    InGame BasesPanel Management
    This is for basesPanel UI.

---
+++ Assets/Scripts/InGame/UI/BasePanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel : MonoBehaviour
+{
+    public InGameObjects InGameObjects;
+
+    public void UpdateLayout()
+    {
+        for(int i = 0; i < 3; ++i)
+        {
+            if(InGameManager.runnerInBases[i+1] != null)
+            {
+                InGameObjects.bases[i].color = InGameManager.currentAttack.teamData.GetData(TeamData.TP.COLOR);
+            }
+            else
+            {
+                InGameObjects.bases[i].color = Colors.primary;
+            }
+        }
+    }
+}
