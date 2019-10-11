bd5ce4f38bdc8ed64311dd2562cab9383aaf944f
challenger71498 <challenger71498@gmail.com>
2019-10-11 16:41:15 +0900

    class Stadium
    팀 구장을 나타내는 class Stadium이 추가되었습니다.

---
+++ Assets/Scripts/GameManagement/Stadium.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stadium
+{
+    public Stadium(string _name, Weather _weather = default)
+    {
+        name = _name;
+        if(_weather == default)
+        {
+            currentWeather = new Weather();
+        }
+        else
+        {
+            currentWeather = _weather;
+        }
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public string name;
+    public Weather currentWeather;
+}
