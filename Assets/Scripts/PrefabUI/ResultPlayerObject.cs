eb14b50ec6f5eb5c45dbc89924615711954f34bb
challenger71498 <challenger71498@gmail.com>
2019-10-11 16:38:27 +0900

    Result Player Panel Prefab fixed
    pos가 잘리는 문제와 소수점 문제, 색상 문제 해결

--- Assets/Scripts/PrefabUI/ResultPlayerObject.cs
+++ Assets/Scripts/PrefabUI/ResultPlayerObject.cs
@@ -6,22 +6,45 @@ using TMPro;
 
 public class ResultPlayerObject : MonoBehaviour
 {
-    public void SetText(Player player)
+    public void SetText(Player player, Game game)
     {
-        numberText.text = ((string)player.playerData.GetData(PlayerData.PP.NUMBER)).ToString();
+        numberText.text = ((int)player.playerData.GetData(PlayerData.PP.NUMBER)).ToString();
         nameText.text = player.playerData.GetData(PlayerData.PP.NAME);
-        positionText.text = player.playerData.GetData(PlayerData.PP.POSITION);
+        positionText.text = Player.positionStringShort[(int)player.playerData.GetData(PlayerData.PP.POSITION)];
         if(player.GetType() == typeof(Batter))
         {
-            //backgroundImage.color
+            if(game.playerOfTheMatch == player)
+            {
+                backgroundImage.color = Colors.redDark;
+            }
+            else
+            {
+                backgroundImage.color = Colors.primaryDark;
+            }
             AB_IPText.text = player.stats.GetStat(PlayerStatistics.PS.AB).ToString();
             R_HText.text = player.stats.GetStat(PlayerStatistics.PS.R).ToString();
             H_ERText.text = player.stats.GetStat(PlayerStatistics.PS.H_BAT).ToString();
             BBText.text = player.stats.GetStat(PlayerStatistics.PS.BB_BAT).ToString();
             RBI_KText.text = player.stats.GetStat(PlayerStatistics.PS.RBI).ToString();
-            //ratingText.text
+            ratingText.text = player.stats.GetStat(PlayerStatistics.PS.RAT).ToString("0.0");
         }
-        
+        if (player.GetType() == typeof(Pitcher))
+        {
+            if (game.playerOfTheMatch == player)
+            {
+                backgroundImage.color = Colors.redDark;
+            }
+            else
+            {
+                backgroundImage.color = Colors.primaryDark;
+            }
+            AB_IPText.text = player.stats.GetStat(PlayerStatistics.PS.IP).ToString("0.#");
+            R_HText.text = player.stats.GetStat(PlayerStatistics.PS.H_PIT).ToString();
+            H_ERText.text = player.stats.GetStat(PlayerStatistics.PS.ER).ToString();
+            BBText.text = player.stats.GetStat(PlayerStatistics.PS.BB_PIT).ToString();
+            RBI_KText.text = player.stats.GetStat(PlayerStatistics.PS.K_PIT).ToString();
+            ratingText.text = player.stats.GetStat(PlayerStatistics.PS.RAT).ToString("0.0");
+        }
     }
 
     public Image backgroundImage;
